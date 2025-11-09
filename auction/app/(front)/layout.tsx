a9f58ded1212b3d6e2d69b12fe954ae1ff17a1ba
Fuma1322 <leonardfuma26@gmail.com>
2025-11-09 19:42:48 +0200

    navbar

---
+++ auction/app/(front)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbarr from '@/components/Frontend/Navbar'
+import React, { ReactNode } from 'react'
+
+
+
+export default async function Layout({children}: {children:ReactNode}) {
+  return (
+     <div className='space-y-2.5 '>
+        <Navbarr />
+       {children}
+       {/* <Footer /> */}
+    </div>
+  )
+}
